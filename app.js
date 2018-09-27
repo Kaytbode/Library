c3140af99b8b5222923f682152896f058f56e356
Kayode <bkaoluborode@gmail.com>
2018-09-27 16:51:01 +0100

    docs: Add static and template files

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.set('views', './src/views/');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Library' });
+});
+
 app.listen(port, () => {
   debug(`listening at port ${chalk.green(port)}`);
 });
